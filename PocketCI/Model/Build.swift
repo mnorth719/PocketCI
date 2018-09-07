d8fb882449bd6eff583a20f7a3bfb22031a4308d
Matt North <matt.north93@gmail.com>
2018-09-06 23:46:52 -0700

    Style cleanup

--- PocketCI/Model/Build.swift
+++ PocketCI/Model/Build.swift
@@ -8,24 +8,16 @@
 
 import Foundation
 
-struct Build : Codable {    
-    let buildNum : Int?
-    let outcome : String?
-    let pushedAt : String?
-    let vcsRevision : String?
+struct Build: Codable {
+    let buildNum: Int?
+    let outcome: String?
+    let pushedAt: String?
+    let vcsRevision: String?
     
     enum CodingKeys: String, CodingKey {
         case buildNum = "build_num"
-        case outcome = "outcome"
+        case outcome
         case pushedAt = "pushed_at"
         case vcsRevision = "vcs_revision"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        buildNum = try values.decodeIfPresent(Int.self, forKey: .buildNum)
-        outcome = try values.decodeIfPresent(String.self, forKey: .outcome)
-        pushedAt = try values.decodeIfPresent(String.self, forKey: .pushedAt)
-        vcsRevision = try values.decodeIfPresent(String.self, forKey: .vcsRevision)
     }
 }
